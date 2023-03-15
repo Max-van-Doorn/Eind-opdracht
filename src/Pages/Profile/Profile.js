1dc0fe23240834efbfaacc7290e1cb086f0fcc3b
Max-van-Doorn <maxvandoorn@ziggo.nl>
2023-03-15 15:02:55 +0100

    10e inzending

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -2,10 +2,12 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthContext';
 import axios from 'axios';
+import './Profile.css'
 
 function Profile() {
     const [profileData, setProfileData] = useState({});
     const {user} = useContext(AuthContext);
+    const { isAuth, logout } = useContext(AuthContext);
 
     useEffect(() => {
         // we halen de pagina-content op in de mounting-cycle
@@ -14,11 +16,19 @@ function Profile() {
             const token = localStorage.getItem('token');
 
             try {
-                const result = await axios.get(' https://frontend-educational-backend.herokuapp.com', {
+                const result = await axios.get(' https://frontend-educational-backend.herokuapp.com/api/user', {
                     headers: {
                         "Content-Type": "application/json",
                         Authorization: `Bearer ${token}`,
-                    },
+                    "id": 3,
+                    "username": "piet",
+                    "email": "piet@novi.nl",
+                    "info": "Ik woon in Utrecht",
+                    "roles": [
+                    "ROLE_USER"
+                ]
+
+            },
                 });
                 setProfileData(result.data);
             } catch (e) {
@@ -30,7 +40,29 @@ function Profile() {
     }, [])
 
     return (
-        <p>hoi!</p>
+        <>
+        <div className="profile-container">
+        <h1 className="profile-text" >Welkom! Je bent nu ingelogd :)</h1>
+            <div className="logout-button-container">
+                <button className="logout-button"
+                        type="button"
+                        onClick={logout}
+                >
+                    Logout
+                </button>
+            </div>
+        </div>
+        <h1>Profielpagina</h1>
+    <section>
+        <h2>Gegevens</h2>
+
+        {/*Waarom werkt dit niet???*/}
+
+        {/*<p><strong>Gebruikersnaam:</strong> {user.username}</p>*/}
+        {/*<p><strong>Email:</strong> {user.email}</p>*/}
+
+    </section>
+            </>
     )
 }
 
